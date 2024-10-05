ff13284c1fec8a55aa7307af487801aea02aa29d
mnr1997 <161993650+mnr1997@users.noreply.github.com>
2024-10-05 15:07:04 +0000

    admin作成/homeコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  namespace :admin do
+    root to: "homes#top"
+  end
+  
   devise_for :customers
   
-  root to: "homes/top"
-  get "homes/about"
+  root to: "homes#top"
+  get "homes/about" => "homes#about"
 end
