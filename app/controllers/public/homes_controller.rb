8effb862ddb8e35f88d701c6cee91e56e2a5c361
mnr1997 <161993650+mnr1997@users.noreply.github.com>
2024-10-13 01:17:44 +0000

    Aboutページ文章

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,5 @@
 class Public::HomesController < PublicController
-  skip_before_action :authenticate_customer!, only: [:top]
+  before_action :authenticate_customer!, except: [:top]
   
   def top
   end
