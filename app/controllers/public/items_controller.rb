8d4c6146d4e2331dba8fbd9fb14941c8446d0c51
mnr1997 <161993650+mnr1997@users.noreply.github.com>
2024-11-26 15:09:00 +0000

    public/item/show

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -3,4 +3,15 @@ class Public::ItemsController < PublicController
     @items = Item.all
     @genres = Genre.all
   end
+  
+  def show
+    @item = Item.find(params[:id])
+    @genres = Genre.all
+  end
+  
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :caption, animal_ids: [])
+  end
 end
