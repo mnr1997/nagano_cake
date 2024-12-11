f688878bf30c817fd403384f6ec6d11a73dadfc1
mnr1997 <161993650+mnr1997@users.noreply.github.com>
2024-12-11 14:19:20 +0000

    public/cart_item:controller,view作成

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
         get "unsubscribe" => "customers#unsubscribe"
         patch "withdraw" => "customers#withdraw"
         resources :addresses, only: [:create, :index, :edit, :update, :destroy]
+        resources :cart_items, only: [:index, :update, :create, :destroy]
       end
     end
   end
