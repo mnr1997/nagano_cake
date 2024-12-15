1900d4a7c4f059ec039bb2411ff9113ab8d60048
mnr1997 <161993650+mnr1997@users.noreply.github.com>
2024-12-15 14:04:16 +0000

    public/item/show/数量選択、カート追加機能

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -21,6 +21,6 @@ class Public::CartItemsController < ApplicationController
   private
   
   def cart_item_params
-    params.require(:cart_item).permit(:amount)
+    params.require(:cart_item).permit(:item_id, :amount)
   end
 end
