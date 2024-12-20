6a0365cf5a17206482934793142e87f699da9f58
mnr1997 <161993650+mnr1997@users.noreply.github.com>
2024-12-20 16:41:32 +0000

    20241220修正

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -7,11 +7,17 @@ class Public::ItemsController < PublicController
   def show
     @item = Item.find(params[:id])
     @genres = Genre.all
+    @cart_item = CartItem.new(cart_item_params)
+    if @cart_item.save
+      redirect_to items_path
+    else
+      render :show
+    end
   end
   
   private
 
-  def post_params
-    params.require(:post).permit(:image, :caption, animal_ids: [])
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :customer_id, :amount)
   end
 end
