196b5b88a84270b5dc80adf47c2ab32978732258
mnr1997 <161993650+mnr1997@users.noreply.github.com>
2024-12-22 14:17:26 +0000

    カート内商品削除

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -8,7 +8,9 @@ class Public::CartItemsController < ApplicationController
   end
   
   def destroy
-    
+    cart_item = CartItem.find(params[:id])
+    cart_item.destroy!
+    redirect_to cart_items_path
   end
   
   def destroy_all
